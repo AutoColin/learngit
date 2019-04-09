3a0a0714dee82ba396335e2f884e0603dd0edec1
AutoColin <colinalwayslol@gmail.com>
2019-04-09 17:19:12 +0800

    remove sth

---
+++ test/zhengfu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int8_t a = 0b01111111;
+    int8_t b = 0b10000000;
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
