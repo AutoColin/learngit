f5b61be10c799068e92e634de7d4c892bdc356f8
AutoColin <colinalwayslol@gmail.com>
2019-04-29 15:27:55 +0800

    delete sth

--- test/convert.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    float num = 0;
-    printf("Please enter the value to convert:");
-    scanf("%f", &num);
-    printf("Convert result:%-5.2f\n", num * 2.54);   
-}
